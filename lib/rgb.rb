68bf7b77c246d22871668a93c880343ed02e84b3
toshi-oliver <igpoid@gmail.com>
2019-11-08 14:50:45 +0900

    create the to_ints method

--- lib/rgb.rb
+++ lib/rgb.rb
@@ -1,3 +1,11 @@
 def to_hex(r, g, b)
   [r, g, b].inject('#') {|hex, n| hex + n.to_s(16).rjust(2, '0')}
 end
+
+def to_ints(hex)
+  r = hex[1..2]
+  g = hex[3..4]
+  b = hex[5..6]
+  ints =  []
+  [r, g, b].map { |s| s.hex }
+end
