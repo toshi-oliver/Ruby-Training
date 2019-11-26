793de1a0f814738b17382df399bcabfe82c2efb7
toshi-oliver <igpoid@gmail.com>
2019-11-26 16:25:57 +0900

    modified the gate_test.rb

--- test/gate_test.rb
+++ test/gate_test.rb
@@ -3,12 +3,21 @@ require './lib/gate'
 require './lib/ticket'
 
 class GateTest < Minitest::Test
-  def test_gate
-    umeda = Gate.new(:umeda)
-    juso = Gate.new(:juso)
-    ticket = Ticket.new(150)
-    umeda.enter(ticket)
-    assert juso.exit(ticket)
+  def set_up
+    @umeda = Gate.new(:umeda)
+    @juso = Gate.new(:juso)
+    @mikuni = Gate.new(:mikuni)
   end
 
+  def test_umeda_to_juso
+    ticket = Ticket.new(150)
+    @umeda.enter(ticket)
+    assert @juso.exit(ticket)
+  end
+
+  def test_umeda_to_mikui_when_fare_is_not_enogh
+    ticket = Ticket.new(150)
+    @umeda.enter(ticket)
+    refute @mikuni.exit(ticket)
+  end
 end
