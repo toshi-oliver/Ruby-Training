9ef9e4d1768dc2070ad07d8fc5623f78fa6a07d9
toshi-oliver <igpoid@gmail.com>
2019-11-26 14:31:49 +0900

    create the ticket.rb

--- test/gate_test.rb
+++ test/gate_test.rb
@@ -1,9 +1,14 @@
 require 'minitest/autorun'
 require './lib/gate'
+require './lib/ticket'
 
 class GateTest < Minitest::Test
   def test_gate
-    assert Gate.new
+    umeda = Gate.new(:umeda)
+    juso = Gate.new(:juso)
+    ticket = Ticket.new(150)
+    umeda.enter(ticket)
+    assert juso.exit(ticket)
   end
 
 end
